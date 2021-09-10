60111715e719d1770ea02ef16821e7212d880313
Roan Franklin <roanfranklin@gmail.com>
2021-09-10 12:08:52 -0300

    update testes.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 3.0"
+   }
+ }
+}
+
 provider "aws" {
   region = var.region
 }
@@ -5,34 +15,13 @@ provider "aws" {
 resource "aws_instance" "maquina_wp" {
   ami = "${var.amis["us-east-1-ubuntu18"]}"
   instance_type = "${var.instance_type.micro}"
-  key_name = "didox-terraform-key"
+  key_name = "myaws-remf-key"
   tags = {
     Name = "maquina_ansible_com_worpress"
   }
   vpc_security_group_ids = ["${aws_security_group.acesso_geral.id}"]
-}
-
-
-resource "null_resource" "before" {
-}
-
-resource "null_resource" "delay" {
-  provisioner "local-exec" {
-    command = "sleep 60"
-  }
-  triggers = {
-    "before" = "${null_resource.before.id}"
-  }
-}
-
-resource "null_resource" "after" {
-  depends_on = ["null_resource.delay"]
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${aws_instance.maquina_wp.public_dns},  ../ansible/provisionar.yml -u ubuntu  --private-key ${var.ssh_key_private.pri}" 
+    command = "ansible-playbook -i ${self.public_dns}, ../ansible/provisionar.yml -u ubuntu --private-key ${var.ssh_key_private.pri} --ssh-common-args='-o StrictHostKeyChecking=no'"
   }
 }
-
-# para liberar a internet interna da maquina, colocar regra do outbound "Outbound rules" como "All traffic"
-# ssh -i ~/.ssh/terraform-aws ubuntu@ec2-54-234-131-240.compute-1.amazonaws.com 
-# conferir 
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -4,7 +4,7 @@ variable "region" {
 
 variable "amis" {
   default = {
-    "us-east-1-ubuntu18" = "ami-013f17f36f8b1fefb"
+    "us-east-1-ubuntu18" = "ami-0747bdcabd34c712a"
     "us-east-1-ubuntu20" = "ami-042e8287309f5df03"
   }
 }
@@ -12,5 +12,10 @@ variable "amis" {
 variable "instance_type" {
   default = {
     "micro" = "t2.micro"
+  }
+}
+variable "ssh_key_private" {
+  default = {
+    "pri" = "~/.ssh/e5be39d5695ea15d98d0b199909cca0a"
   }
 }
